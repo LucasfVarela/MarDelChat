ba11a27f8d54f5da8f259a0f5b9af9a15755c61f
NicolasFrancolino <87394098+NicolasFrancolino@users.noreply.github.com>
2022-06-18 14:42:49 -0300

    Ok

---
+++ API_DataCore/PluginInterfaces/IContactoRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API_CoreBusiness.Entities;
+
+namespace API_GenericCore.GenericRepository.Interfaces
+{
+    internal interface IContactoRepository : IGenericRepository<Contactos>
+    {
+    }
+}
