b4e2a36d7e487b3fcde4a32ef57f575f5c24676d
NicolasFrancolino <87394098+NicolasFrancolino@users.noreply.github.com>
2022-06-21 19:03:19 -0300

    update

--- MarDelChat/Controllers/ChatController.cs
+++ MarDelChat/Controllers/ChatController.cs
@@ -9,46 +9,44 @@ namespace MarDelChat.Controllers
     [ApiController]
     public class ChatController : Controller
     {
-        private readonly IUnitOfWork _context;
-        private readonly ILogger<ChatController> _logger;
-        private CustomLogger loggerCustom { get; set; }
+        private readonly IUnitOfWork context;
+        private readonly ILogger<ChatController> logger;
+        private CustomLogger customLogger { get; set; }
 
         public ChatController(IUnitOfWork context, ILogger<ChatController> logger)
         {
-            _context = context;
-            _logger = logger;
-            loggerCustom = new CustomLogger(_logger);
+            this.context = context;
+            this.logger = logger;
+            customLogger = new CustomLogger(logger);
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<Chat>> Get()
         {
-            loggerCustom.Info("[Get] Chat");
-            var entidadaux = _context.ChatRepo.GetAll();
+            customLogger.Info("[Get] Chat");
+            var entidadaux = context.ChatRepo.GetAll();
             return Ok(entidadaux);
         }
-
-        public ActionResult CrearChat([FromBody] Chat chat)
+        [HttpPost]
+        public ActionResult Post([FromBody] Chat chat)
         {
-            loggerCustom.Info("[Post] Chat");
-            _context.ChatRepo.Insert(chat);
-            _context.Save();
+            customLogger.Info("[Post] Chat");
+            context.ChatRepo.Insert(chat);
+            context.Save();
             return Ok();
         }
 
         [HttpDelete("{id}")]
-        public ActionResult EliminarChat(int id)
+        public ActionResult Delete(int id)
         {
-            var entidadaux = _context.ChatRepo.GetById(id);
-          
-            loggerCustom.Info("[Delete] Chat");
-            _context.ChatRepo.Delete(id);
-            _context.Save();
+            var entidadaux = context.ChatRepo.GetById(id);
+
+            customLogger.Info("[Delete] Chat");
+            context.ChatRepo.Delete(id);
+            context.Save();
             return Ok();
         }
     }
-
-
 
 }
 
--- MarDelChat/Controllers/WeatherForecastController.cs
+++ MarDelChat/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MarDelChat.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
